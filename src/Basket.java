568c3085100cc9d19b03266d8595bc3208058e57
Chudov Ilya <chudow-iluha@mail.ru>
2022-10-11 22:52:16 +0300

    small changes

--- src/Basket.java
+++ src/Basket.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Basket {
-    String[] products;
-    int[] prices;
-    int[] basketCount;
+    private String[] products;
+    private int[] prices;
+    private int[] basketCount;
 
     public Basket(String[] products, int[] prices, int[] basketCount) {
         this.products = products;
